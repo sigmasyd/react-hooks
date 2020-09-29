5f56890575db378a7bfc5f3fc6879d4dfddba87b
Carlos Fernando Barcos Sinche <fbarcos8@hotmail.com>
2020-09-29 13:42:12 -0500

    UseEffect basico...
    	nuevo archivo:  src/components/02-useEffect/SimpleForm.js
    	nuevo archivo:  src/components/02-useEffect/effects.css
    	modificado:     src/index.js

---
+++ src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import './effects.css';
+
+export const SimpleForm = () => {
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+  });
+
+  const { name, email } = formState;
+
+  // useEffect, permite ejecutar un efecto secundario cuando algo suceda en los componentes
+  useEffect(() => {
+    console.log('hey');
+  }, []);
+  useEffect(() => {
+    console.log('formstate cambio');
+  }, [formState]);
+  useEffect(() => {
+    console.log('email cambio');
+  }, [email]);
+  const handleInputChange = ({ target }) => {
+    //console.log(target.name);
+    //console.log(target.value);
+    setFormState({
+      ...formState,
+      [target.name]: target.value,
+    });
+  };
+  return (
+    <>
+      <h1>UseEffect</h1>
+      <hr />
+      <div className="form-group">
+        <input
+          type="text"
+          name="name"
+          className="form-control"
+          placeholder="Tu nombre"
+          autoComplete="off"
+          value={name}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          name="email"
+          className="form-control"
+          placeholder="email@domain.com"
+          autoComplete="off"
+          value={email}
+          onChange={handleInputChange}
+        />
+      </div>
+    </>
+  );
+};

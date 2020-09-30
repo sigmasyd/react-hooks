a7e429b8e72ad8c7b8d8b10eb2ac7e8495b8a712
Carlos Fernando Barcos Sinche <fbarcos8@hotmail.com>
2020-09-30 10:31:15 -0500

    Se establece una funcion de limpieza para remover el listener y evitar consumo innecesario de memoria
    	modificado:     src/components/02-useEffect/Message.js

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,15 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const Message = () => {
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+  const { x, y } = coords;
+
   useEffect(() => {
     console.log('Componente montado');
+    const mouseMove = (e) => {
+      const coors = {
+        x: e.x,
+        y: e.y,
+      };
+      console.log(coors);
+      setCoords(coors);
+    };
+    window.addEventListener('mousemove', mouseMove);
     return () => {
+      window.removeEventListener('mousemove', mouseMove);
       console.log('Componente desmontado');
     };
   }, []);
   return (
     <>
       <h3>Eres genial</h3>
+      <p>
+        x:{x} y:{y}
+      </p>
     </>
   );
 };

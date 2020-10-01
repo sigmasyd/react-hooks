c6c4fd4395a49512123c457c827ea2db9f5ece90
Carlos Fernando Barcos Sinche <fbarcos8@hotmail.com>
2020-09-30 22:23:52 -0500

    Se usa un custom hook useForm para organizar el codigo
    	nuevo archivo:  src/components/02-useEffect/FormWithCustomHook.js
    	nuevo archivo:  src/hooks/useForm.js
    	modificado:     src/index.js

---
+++ src/components/02-useEffect/FormWithCustomHook.js
@@ -0,0 +1,60 @@
+import React, { useEffect } from 'react';
+import { useForm } from '../../hooks/useForm';
+import './effects.css';
+
+export const FormWithCustomHook = () => {
+  const [formValues, handleInputChange] = useForm({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const { name, email, password } = formValues;
+
+  useEffect(() => {
+    console.log('email cambio');
+  }, [email]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formValues);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Form With Custom Hook</h1>
+      <hr />
+      <div className="form-group">
+        <input
+          type="text"
+          name="name"
+          className="form-control"
+          placeholder="Tu nombre"
+          autoComplete="off"
+          value={name}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          name="email"
+          className="form-control"
+          placeholder="email@domain.com"
+          autoComplete="off"
+          value={email}
+          onChange={handleInputChange}
+        />
+        <input
+          type="password"
+          name="password"
+          className="form-control"
+          placeholder="*******"
+          value={password}
+          onChange={handleInputChange}
+        />
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Guardar
+      </button>
+    </form>
+  );
+};
